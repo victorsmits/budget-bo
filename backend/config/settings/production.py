2a0f1e27ed52e17d8a9bc8bae3fd52b51fc72cdf
victorsmits <contact@victorsmits.com>
2026-03-15 13:54:14 +0100

    update serve static

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -4,5 +4,8 @@ DEBUG = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-# Serve static files via Django in production
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+# Serve static files via Django in production using whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+# Add whitenoise middleware at the top
+MIDDLEWARE.insert(0, 'whitenoise.middleware.WhiteNoiseMiddleware')
